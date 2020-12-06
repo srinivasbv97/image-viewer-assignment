1d1eba33a282a4338702f4310a4ddfa97dac6912
srinivasbv97 <65945543+srinivasbv97@users.noreply.github.com>
2020-12-07 00:22:28 +0530

    Code Corrections

---
+++ src/common/utils.js
@@ -0,0 +1,5 @@
+export const constants = {
+  mediaIdUrl:"https://graph.instagram.com/me/media?fields=id,caption",
+  userMediaUrl:"https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp,caption",
+
+}
